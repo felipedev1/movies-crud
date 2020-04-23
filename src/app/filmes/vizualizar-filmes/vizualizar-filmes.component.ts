5c838f9f605f3918588c5cd36796c7c7d36a7e75
Felipe <felipe.pereira.dev@gmail.com>
2020-04-23 16:06:38 -0300

    delete method

--- src/app/filmes/vizualizar-filmes/vizualizar-filmes.component.ts
+++ src/app/filmes/vizualizar-filmes/vizualizar-filmes.component.ts
@@ -1,7 +1,10 @@
+import { MatDialog } from '@angular/material';
 import { Filme } from './../../shared/models/filme';
 import { FilmesService } from './../../core/filmes.service';
 import { Component, OnInit } from '@angular/core';4
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Alerta } from 'src/app/shared/models/alerta';
+import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
 
 @Component({
   selector: 'dio-vizualizar-filmes',
@@ -11,17 +14,39 @@ import { ActivatedRoute } from '@angular/router'
 export class VizualizarFilmesComponent implements OnInit {
 
   filme: Filme
+  id: number
   
   readonly semFoto = 'https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg'
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private fimesService: FilmesService) { }
+  constructor(public dialog: MatDialog,
+              private activatedRoute: ActivatedRoute,
+              private filmesService: FilmesService,
+              private router: Router) { }
 
   ngOnInit() {
-    this.vizualizar(this.activatedRoute.snapshot.params['id'])
+    this.id = this.activatedRoute.snapshot.params['id']
+    this.vizualizar()
   }
 
-  private vizualizar(id: number): void {
-    this.fimesService.vizualizar(id).subscribe((filme: Filme)=> this.filme = filme)
+  excluir(): void {
+    const config = {
+      data: {
+        titulo: 'Você tem certeza que deseja excluir?',
+        descricao: 'Caso você tenha certeza que deseja excluir, clique no botão OK',
+        corBtnCancelar: 'primary',
+        corBtnSucesso: 'warn',
+        possuiBtnFechar: true,
+      } as Alerta
+    }
+    const dialogRef = this.dialog.open(AlertaComponent, config)
+    dialogRef.afterClosed().subscribe((opcao: boolean)=>{
+      if(opcao){
+        this.filmesService.excluir(this.id).subscribe(()=> this.router.navigateByUrl('/filmes'))
+      }
+    })
+  }
+
+  private vizualizar(): void {
+    this.filmesService.vizualizar(this.id).subscribe((filme: Filme)=> this.filme = filme)
   }
 }
